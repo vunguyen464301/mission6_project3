16edfd25264bb945c12c3202f78397ec6abc00d7
unknown <vunguyen464301@gmail.com>
2019-08-27 17:48:05 +0700

    add more console.log

---
+++ pages/post.js
@@ -0,0 +1,10 @@
+import { useRouter } from 'next/router'
+
+const Post = () => {
+  const router = useRouter();
+  const { name } = router.query;
+
+  return <p>My name is : {name}</p>
+}
+
+export default Post;
